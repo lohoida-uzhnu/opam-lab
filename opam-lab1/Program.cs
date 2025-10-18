aa557c7067f33f351ce4af9cc4f97a2357a90b2e
lohoida-uzhnu <c.logoida.stanislav@student.uzhnu.edu.ua>
2025-10-18 19:53:23 +0300

    add service price

--- opam-lab1/Program.cs
+++ opam-lab1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks.Sources;
+﻿using System;
 
 namespace opam_lab1;
 
@@ -7,11 +7,19 @@ class Program
     static void Main(string[] args)
     {
         Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("=== Вітаємо до Медичниго центру Сім'я ===");
-        Console.WriteLine("=== Меню послуг ===");
-        Console.WriteLine("1. Запис на прийом до Окуліста");
-        Console.WriteLine("2. Запис на прийом до Уролога");
-        Console.WriteLine("3. Запис на прийом до Невролога");
-        Console.WriteLine("4. Запис на прийом до Нарколога");
+        Console.WriteLine("\n=== Вітаємо до Медичниго центру Сім'я ===");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("\n=== Меню послуг ===");
+        Console.WriteLine("1. Запис на прийом до Окуліста - 800грн.");
+        Console.WriteLine("2. Запис на прийом до Уролога - 1000грн.");
+        Console.WriteLine("3. Запис на прийом до Невролога - 900грн.");
+        Console.WriteLine("4. Запис на прийом до Нарколога - 1200грн.");
+
+        double priceOphthalmologist = 800;
+        double priceUrologist = 1000;
+        double priceNeurologist = 900;
+        double priceNarcologist = 1200;
     }
 }
