a8562ec625cb644b7801abd9c6b181fb20535523
lohoida-uzhnu <c.lohoida.stanislav@student.uzhnu.edu.ua>
2025-11-07 21:50:37 +0200

    add SpecialistMenu, BestSpecialist and SearchMenu

--- opam-lab1/Program.cs
+++ opam-lab1/Program.cs
@@ -60,12 +60,13 @@ class Program
                 ShowServiceMenu();
                 break;
             case 3:
-                //ShowSpecialistMenu();
+                ShowSpecialistMenu();
                 break;
             case 4:
-                //ShowBestSpecialist();
+                ShowBestSpecialist();
                 break;
             case 5:
+                Console.WriteLine("Функція в розробці... Повертаємо в головне меню.");
                 break;
                 case 6:
                 Console.WriteLine("Вихід з програми. До побачення!");
@@ -78,7 +79,55 @@ class Program
                 break;
         }
     }
-    private static void ShowServiceMenu()
+
+    private static void ShowAppointmentMenu()
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("\n=== Меню послуг ===");
+        Console.WriteLine("1. Запис на прийом до Окуліста - 800грн.");
+        Console.WriteLine("2. Запис на прийом до Уролога - 1000грн.");
+        Console.WriteLine("3. Запис на прийом до Невролога - 900грн.");
+        Console.WriteLine("4. Запис на прийом до Нарколога - 1200грн.");
+
+        double priceOphthalmologist = 800;
+        double priceUrologist = 1000;
+        double priceNeurologist = 900;
+        double priceNarcologist = 1200;
+
+        Console.WriteLine("\nКількість прийомів до окуліста: ");
+        int countOphthalmologist = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Кількість прийомів до уролога: ");
+        int countUrologist = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Кількість прийомів до невролога: ");
+        int countNeurologist = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Кількість прийомів до нарколога: ");
+        int countNarcologist = Convert.ToInt32(Console.ReadLine());
+
+        double totalOphthalmologist = priceOphthalmologist * countOphthalmologist;
+        double totalUrologist = priceUrologist * countUrologist;
+        double totalNeurologist = priceNeurologist * countNeurologist;
+        double totalNarcologist = priceNarcologist * countNarcologist;
+
+        double totalPrice = totalOphthalmologist + totalUrologist + totalNeurologist + totalNarcologist;
+
+        Random random = new Random();
+        double discount = Math.Round(random.NextDouble() * 15, 2);
+        double discountAmount = totalPrice * (discount / 100);
+        double finalPrice = totalPrice - discountAmount;
+
+        double finalPricePow = Math.Pow(finalPrice, 2);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"\n=== Загальна вартість прийомів ===: {totalPrice} грн.");
+        Console.WriteLine($"Ваша знижка: {discount}% що становить {discountAmount} грн.");
+        Console.WriteLine($"До сплати після знижки: {finalPrice} грн.");
+        Console.WriteLine($"Фінальна ціна в квадраті: {finalPricePow} грн.");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\nДякуємо, що обрали Медичний центр Сім'я! Бажаємо Вам міцного здоров'я!");
+    }
+
+   private static void ShowServiceMenu()
     {
         Console.WriteLine("===== Меню наших послуг =====");
         Console.WriteLine("1. Додати нову послугу");
@@ -105,53 +154,19 @@ class Program
         }
     }
 
-    private static void ShowAppointmentMenu()
-        {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine("\n=== Меню послуг ===");
-        Console.WriteLine("1. Запис на прийом до Окуліста - 800грн.");
-        Console.WriteLine("2. Запис на прийом до Уролога - 1000грн.");
-        Console.WriteLine("3. Запис на прийом до Невролога - 900грн.");
-        Console.WriteLine("4. Запис на прийом до Нарколога - 1200грн.");
-
-        double priceOphthalmologist = 800;
-        double priceUrologist = 1000;
-        double priceNeurologist = 900;
-        double priceNarcologist = 1200;
-
-        Console.WriteLine("\nКількість прийомів до окуліста: ");
-        int countOphthalmologist = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Кількість прийомів до уролога: ");
-        int countUrologist = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Кількість прийомів до невролога: "); 
-        int countNeurologist = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Кількість прийомів до нарколога: ");
-        int countNarcologist = Convert.ToInt32(Console.ReadLine());
-
-        double totalOphthalmologist = priceOphthalmologist * countOphthalmologist;
-        double totalUrologist = priceUrologist * countUrologist;
-        double totalNeurologist = priceNeurologist * countNeurologist;
-        double totalNarcologist = priceNarcologist * countNarcologist;
-
-        double totalPrice = totalOphthalmologist + totalUrologist + totalNeurologist + totalNarcologist;
-
-        Random random = new Random();
-        double discount = Math.Round(random.NextDouble() * 15, 2);
-        double discountAmount = totalPrice * (discount / 100);
-        double finalPrice = totalPrice - discountAmount;
-
-        double finalPricePow = Math.Pow(finalPrice, 2);
-
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"\n=== Загальна вартість прийомів ===: {totalPrice} грн.");
-        Console.WriteLine($"Ваша знижка: {discount}% що становить {discountAmount} грн."); 
-        Console.WriteLine($"До сплати після знижки: {finalPrice} грн.");
-        Console.WriteLine($"Фінальна ціна в квадраті: {finalPricePow} грн.");
-        Console.ResetColor();
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("\nДякуємо, що обрали Медичний центр Сім'я! Бажаємо Вам міцного здоров'я!");
-
-        // creat by kudaihashish
-        // github.com/lohoida-uzhnu
+    private static void ShowSpecialistMenu()
+    {
+        Console.WriteLine("Функція в розробці... Повертаємо в головне меню.");
+        ShowMainMenu();
+        return;
     }
+
+    private static void ShowBestSpecialist()
+    {
+        Console.WriteLine("Функція в розробці... Повертаємо в головне меню.");
+        ShowMainMenu();
+        return;
+    }
+    // creat by kudaihashish
+    // github.com/lohoida-uzhnu
 }
